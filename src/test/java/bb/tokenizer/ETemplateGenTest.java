bf996491377889c65982d217127a151f3af49532
eddyim <edwardim@berkeley.edu>
2017-06-20 13:30:04 -0700

    Tests for Template Generator

---
+++ src/test/java/bb/tokenizer/ETemplateGenTest.java
@@ -0,0 +1,28 @@
+package bb.tokenizer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by eim on 6/20/2017.
+ */
+public class ETemplateGenTest {
+
+    @Test
+    public void testPackageStatement() {
+        String test1 = "bb/egen/demo";
+        String test2 = "./java/bb/egen/demo";
+        assertEquals("package bb.egen.demo;", ETemplateGen.getPackageStatement(test1));
+        assertEquals("package bb.egen.demo;", ETemplateGen.getPackageStatement(test2));
+
+    }
+
+    @Test
+    public void testStatementStrings() {
+        ITokenizer tokenizer = new ETokenizer();
+        Token myToken = tokenizer.tokenize("<% \"%>\n\" %>").get(0);
+        String test1 = ETemplateGen.getNextToken(myToken);
+        System.out.println(test1);
+    }
+}
