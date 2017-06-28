c57e31f41a7727c08d8b0d967c14e812efba1eec
eddyim <edwardim@berkeley.edu>
2017-06-28 11:15:07 -0700

    Commiting java files

--- src/test/java/bb/egen/demo/Bootstrap.java
+++ src/test/java/bb/egen/demo/Bootstrap.java
@@ -1,26 +1,25 @@
 package bb.egen.demo;
-
 import java.io.IOException;
+public class Bootstrap  {
 
-public class Bootstrap {
-
+private static Bootstrap INSTANCE = new Bootstrap();
     public static String render() {
         StringBuilder sb = new StringBuilder();
         renderInto(sb);
         return sb.toString();
     }
 
-    public static void renderInto(Appendable buffer) {
-        try {
-            buffer.append("<html>\n<body>\n<p>1 + 1 = ");
-            buffer.append(toS(1 + 1));
-            buffer.append("</p>\n</body>\n</html>");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private static String toS(Object o) {
         return o == null ? "" : o.toString();
+    }
+
+     public static void renderInto(Appendable buffer) {INSTANCE.renderImpl(buffer);}    public void renderImpl(Appendable buffer) {
+        try {
+            buffer.append("<html>\n<body>\n<h1>This is a demo template</h1>\n<p>1 + 1 = ");
+            buffer.append(toS(1 + 1));
+            buffer.append("</p>\n</body>\n</html>");
+} catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
