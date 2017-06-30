91af934f20de06114b70cddf0875bfa77bf764c1
carson <carsongross@gmail.com>
2017-06-30 12:18:48 -0700

    layout example code

--- src/main/java/bb/runtime/ILayout.java
+++ src/main/java/bb/runtime/ILayout.java
@@ -1,9 +1,11 @@
 package bb.runtime;
 
+import java.io.IOException;
+
 public interface ILayout {
 
-    void header(Appendable buffer);
+    void header(Appendable buffer) throws IOException;
 
-    void footer(Appendable buffer);
+    void footer(Appendable buffer) throws IOException;
 
 }
