8524cdd23ed11f22517ee5dae34014b0aaa1470e
eddyim <edwardim@berkeley.edu>
2017-07-07 09:58:50 -0700

    hyg carson

---
+++ spark-template-bb/src/main/java/bb/codegen/TemplateGenerator.java
@@ -0,0 +1,11 @@
+package bb.codegen;
+
+import bb.codegen.EFileScanner;
+/**
+ * Created by eim on 7/6/2017.
+ */
+public class TemplateGenerator {
+    public static void main(String[] args) {
+        EFileScanner.main(args);
+    }
+}
