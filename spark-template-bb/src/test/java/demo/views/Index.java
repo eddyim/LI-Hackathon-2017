80c9ab407e180cad3bf8a99eca107d80f586472f
Harika Kalidhindi <hkalidhindi@guidewire.com>
2017-07-10 12:53:42 -0700

    save

--- spark-template-bb/src/test/java/demo/views/Index.java
+++ spark-template-bb/src/test/java/demo/views/Index.java
@@ -10,12 +10,9 @@ private static Index INSTANCE = new Index();
         return sb.toString();
     }
 
-     public static void renderInto(Appendable buffer,List<Message> messages, String user) {INSTANCE.renderImpl(buffer,messages,user);}
-
-     public void renderImpl(Appendable buffer,List<Message> messages, String user) {
+     public static void renderInto(Appendable buffer,List<Message> messages, String user) {INSTANCE.renderImpl(buffer,messages,user);}    public void renderImpl(Appendable buffer,List<Message> messages, String user) {
         try {
-            Layout.asLayout().header(buffer);
-            buffer.append("\n");
+Layout.asLayout().header(buffer);            buffer.append("\n");
             buffer.append("\n");
             buffer.append("\n");
             buffer.append("\n");
@@ -23,12 +20,14 @@ private static Index INSTANCE = new Index();
             buffer.append(toS(user));
             buffer.append("\n        </div>\n    </div>\n\n    <div id=\"messages\">\n        <div class=\"message\">\n            Demo Message\n        </div>\n        ");
             for (Message m: messages) {
-            buffer.append("\n        <div class=\"message\">\n            ");
+            buffer.append("\n        <div class=\"message\">\n            <span style=\"font-weight: bold\">");
+            buffer.append(toS(m.getSender()));
+            buffer.append(": </span>");
             buffer.append(toS(m.getMessage()));
             buffer.append("\n        </div>\n        ");
             }
-            buffer.append("\n    </div>\n\n    <div id=\"chat-box\">\n        <form action=\"/\" method=\"post\" autocomplete=\"off\">\n        <input type=\"text\" name=\"message\" autofocus=\"autofocus\">\n            <button>Submit</button>\n        </form>\n    </div>\n</div>");
-            Layout.asLayout().footer(buffer);} catch (Exception e) {
+            buffer.append("\n    </div>\n\n    <div id=\"chat-box\">\n        <form action=\"/\" method=\"post\" autocomplete=\"off\">\n        <input type=\"text\" name=\"message\" size=\"100\" autofocus=\"autofocus\">\n            <button>Submit</button>\n        </form>\n    </div>\n</div>");
+Layout.asLayout().footer(buffer);} catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
