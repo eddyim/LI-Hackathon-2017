f46b93c1f892cb42a6e9a72aa34c094a15e6cf71
Harika Kalidhindi <hkalidhindi@guidewire.com>
2017-07-11 11:03:56 -0700

    changed extends

--- spark-template-bb/src/test/java/demo/views/Index.java
+++ spark-template-bb/src/test/java/demo/views/Index.java
@@ -10,25 +10,39 @@ private static Index INSTANCE = new Index();
         return sb.toString();
     }
 
-     public static void renderInto(Appendable buffer,List<Message> messages, String user) {INSTANCE.renderImpl(buffer,messages,user);}    public void renderImpl(Appendable buffer,List<Message> messages, String user) {
+     public static void renderInto(Appendable buffer,List<Message> messages, String user) {
+        INSTANCE.renderImpl(buffer,messages,user);}
+
+     public void renderImpl(Appendable buffer,List<Message> messages, String user) {
         try {
-Layout.asLayout().header(buffer);            buffer.append("\n");
+            Layout.asLayout().header(buffer);
             buffer.append("\n");
             buffer.append("\n");
             buffer.append("\n");
-            buffer.append("\n\n<div id=\"outer-frame\">\n    <div id=\"who\">\n        <ul>\n            <li>Harika</li>\n            <li>Ed</li>\n            <li>Carson</li>\n        </ul>\n    </div>\n\n    <div id=\"top-bar\">\n        <div id=\"title\">\n        Welcome to internchan, ");
+            buffer.append("\n");
+            buffer.append("\n\n<div id=\"outer-frame\">\n    <div id=\"who\">\n        <ul>" +
+                    "<li>Harika</li>\n            <li>Ed</li>\n            <li>Carson</li>" +
+                    "</ul>\n    </div>\n\n    <div id=\"top-bar\">\n        <div id=\"title\">" +
+                    "Welcome to internchan, ");
             buffer.append(toS(user));
-            buffer.append("\n        </div>\n    </div>\n\n    <div id=\"messages\" ic-src=\"/messages\" ic-poll=\"1s\">\n        ");
+            buffer.append("\n        </div>\n    </div>\n" +
+                    "<div id=\"messages\" ic-src=\"/messages\" ic-poll=\"1s\">\n        ");
             for (Message m: messages) {
-            buffer.append("\n        <div class=\"message\">\n            <span style=\"font-weight: bold\">");
+            buffer.append("\n        <div class=\"message\">" +
+                    "<span style=\"font-weight: bold\">");
             buffer.append(toS(m.getSender()));
             buffer.append(": </span>");
             buffer.append(toS(m.getMessage()));
             buffer.append("\n        </div>\n        ");
             }
-            buffer.append("\n    </div>\n\n    <div id=\"chat-box\">\n        <form action=\"/\" method=\"post\" autocomplete=\"off\">\n        <textarea id=\"input-box\" type=\"text\" name=\"message\" wrap=\"hard\" rows =\"3\" cols=\"90\" autofocus=\"autofocus\"></textarea>\n            <button ic-post-to=\"/\">Submit</button>\n        </form>\n    </div>\n</div>");
-Layout.asLayout().footer(buffer);} catch (Exception e) {
+            buffer.append("\n    </div>\n\n    <div id=\"chat-box\">" +
+                    "<form action=\"/\" method=\"post\" autocomplete=\"off\">" +
+                    "<textarea id=\"input-box\" type=\"text\" name=\"message\" " +
+                    "wrap=\"hard\" rows =\"3\" cols=\"90\" autofocus=\"autofocus\"></textarea>" +
+                    " <button ic-post-to=\"/\">Submit</button>\n        </form>\n    </div>\n</div>");
+            Layout.asLayout().footer(buffer);} catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 }
+
