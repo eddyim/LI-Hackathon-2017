08a6c652c092818fa1a0022c89befd517fa3cdcc
hkalidhindi <jrharika@gmail.com>
2017-07-14 14:14:00 -0700

    expr tests

--- bb-test/src/test/java/bb/expressions/ExpressionsTest.java
+++ bb-test/src/test/java/bb/expressions/ExpressionsTest.java
@@ -9,8 +9,12 @@ public class ExpressionsTest {
 
     @Test
     public void basicExpressionsWork() {
-        assertEquals("2", SimpleExpression1.render());
-        assertEquals("2", SimpleExpression2.render());
+        assertEquals("2", SimpleExpressionOrig.render());
+        assertEquals("2", SimpleExpressionAlt.render());
+        assertEquals("5", VariableExpressionOrig.render());
+        assertEquals("5", VariableExpressionAlt.render());
+        assertEquals("16", MethodCallExpressionOrig.render());
+        assertEquals("16", MethodCallExpressionAlt.render());
     }
 
 }
